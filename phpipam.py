5eaa930222277c501105c6257d40232ca16917e1
Rudy Hardeman <zarya@gigafreak.net>
2015-03-12 21:11:22 +0000

    Refactored some code

--- phpipam.py
+++ phpipam.py
@@ -5,8 +5,9 @@ from mcrypt import MCRYPT
 import iptools
 from dns import reversename
 
+
 class PHPipam:
-    def __init__(self,url,app_id,app_key):
+    def __init__(self, url, app_id, app_key):
         self.url = url
         self.app_id = app_id
         self.app_key = app_key
@@ -15,104 +16,129 @@ class PHPipam:
         data['format'] = "json"
         data['controller'] = "addresses"
         data['action'] = "read"
-        c = MCRYPT('rijndael-256','ecb')
+
+        c = MCRYPT('rijndael-256', 'ecb')
         c.init(self.app_key)
+
         request = json.dumps(data)
         enc_request = bytes(c.encrypt(request))
+
         req = {
-            'enc_request':base64.b64encode(enc_request),
-            'app_id':self.app_id
+            'enc_request': base64.b64encode(enc_request),
+            'app_id': self.app_id
         }
+
         r = requests.post("%s/api/" % self.url, data=req)
         response = r.json
+
         if not response['success']:
             return False
+
         return response['data']
 
-    def lookup(self,fqdn):
+    def lookup(self, fqdn):
+        hosts = []
         if fqdn.endswith('in-addr.arpa'):
             ip = fqdn.split('.')
+
             if len(ip) != 6:
                 return False
-            _ip = "%s.%s.%s.%s" %(ip[3],ip[2],ip[1],ip[0])
-            req = {'ip':_ip}
+
+            req = {
+                'ip': "%s.%s.%s.%s" % (ip[3], ip[2], ip[1], ip[0]) 
+            }
+
         elif fqdn.endswith('ip6.arpa'):
             ip_rev = fqdn[:-9]
             ip_tmp = ip_rev[::-1].replace(".","")
-            ip = ":".join([ip_tmp[j:j+4] for j in range(0,len(ip_tmp),4)])
-            req = {'iplong':str(iptools.ipv6.ip2long(ip)) }
+            ip = ":".join([ip_tmp[j:j+4] for j in range(0, len(ip_tmp), 4)])
+
+            req = {
+                'iplong': str(iptools.ipv6.ip2long(ip))
+            }
+
         else:
-            req = {'dns_name':fqdn}
+            req = {
+                'dns_name':fqdn
+            }
         
         resp = self.request(req)
+
         if not resp:
             return False
-        hosts = []
+
         for host in resp:
-            try:
+            if int(host['ip_addr']) <= 4294967295: 
                 hosts.append({
                     'host':host['dns_name'],
                     'ip':iptools.ipv4.long2ip(int(host['ip_addr']))})
-            except:
+            else:
                 hosts.append({
                     'host':host['dns_name'],
                     'ip6':iptools.ipv6.long2ip(int(host['ip_addr']))})
-                continue
 
         return hosts
 
-    def zone(self,zone):
+    def zone(self, zone):
+        hosts = []
+
         if zone.endswith('in-addr.arpa'):
             #reverse zone
             ip = zone.split('.')
+
             if len(ip) == 3:
-                ip_from = "%s.0.0.0"%(ip[0])
+                ip_from = "%s.0.0.0" % (ip[0])
                 ip_to = "%s.255.255.255"%(ip[0])
             elif len(ip) == 4:
-                ip_from = "%s.%s.0.0"%(ip[1],ip[0])
-                ip_to = "%s.%s.255.255"%(ip[1],ip[0])
+                ip_from = "%s.%s.0.0" % (ip[1], ip[0])
+                ip_to = "%s.%s.255.255" % (ip[1], ip[0])
             elif len(ip) == 5:
-                ip_from = "%s.%s.%s.0"%(ip[2],ip[1],ip[0])
-                ip_to = "%s.%s.%s.255"%(ip[2],ip[1],ip[0])
+                ip_from = "%s.%s.%s.0" % (ip[2], ip[1], ip[0])
+                ip_to = "%s.%s.%s.255" % (ip[2], ip[1], ip[0])
             else:
                 return False
+
             req = {
-                'ip_from':ip_from,
-                'ip_to':ip_to
+                'ip_from': ip_from,
+                'ip_to': ip_to
             }
+
         elif zone.endswith('ip6.arpa'):
             ip_rev = zone[:-9]
             ip_tmp = ip_rev[::-1].replace(".","")
-            ip = ":".join([ip_tmp[j:j+4] for j in range(0,len(ip_tmp),4)])
+            ip = ":".join([ip_tmp[j:j+4] for j in range(0, len(ip_tmp), 4)])
             bits = len(ip_rev.split('.')) * 4
-            iprange = iptools.ipv6.cidr2block("%s::/%s"%(ip,bits))
+            iprange = iptools.ipv6.cidr2block("%s::/%s"%(ip, bits))
+
             req = {
                 'iplong_from': str(iptools.ipv6.ip2long(iprange[0])),
                 'iplong_to': str(iptools.ipv6.ip2long(iprange[1]))
             }
+
         else:
-            #forward zone
             query = "%." + zone
             req = {'dns_name': query} 
+
         resp = self.request(req)
+
         if not resp:
             return False
-        hosts = []
+
         for host in resp:
-            try:
+            if int(host['ip_addr']) <= 4294967295: 
                 hosts.append({
-                    'host':host['dns_name'],
-                    'ip':iptools.ipv4.long2ip(int(host['ip_addr']))})
-            except:
+                    'host': host['dns_name'],
+                    'ip': iptools.ipv4.long2ip(int(host['ip_addr']))})
+            else: 
                 ip6 = iptools.ipv6.long2ip(int(host['ip_addr'])) 
                 reverse_octets = str(ip6).split(':')[::-1]
                 ip6_rev = '.'.join(reverse_octets) + '.in-addr.arpa'
-                ip6_rev = reversename.from_address(ip6) 
+                ip6_rev = reversename.from_address(ip6)
+
                 hosts.append({
-                    'host':host['dns_name'],
-                    'ip6':iptools.ipv6.long2ip(int(host['ip_addr'])),
-                    'ip6_rev':ip6_rev
+                    'host': host['dns_name'],
+                    'ip6': iptools.ipv6.long2ip(int(host['ip_addr'])),
+                    'ip6_rev': ip6_rev
                 })
-                continue
 
         return hosts
--- powerdns-pipe.py
+++ powerdns-pipe.py
@@ -27,40 +27,66 @@ while 1:
     except KeyboardInterrupt:
         break
 
-    line = line.rstrip('\r\n')
+    line = line.rstrip("\r\n")
     data = line.split("\t")
+
     if len(data) < 2:
         continue
-    api = PHPipam(config.get('phpipam','url'),config.get('phpipam','app_id'),config.get('phpipam','app_key'))
+
+    api = PHPipam(
+        url=config.get('phpipam','url'),
+        app_id=config.get('phpipam','app_id'),
+        app_key=config.get('phpipam','app_key')
+    )
+
     logging.debug(line)
+
     if data[0] == "HELO":
-        responder("OK\tpython phpIPAM Backend Module ver 0.1 (PID %s)"%(os.getpid()))
+        responder("OK\tpython phpIPAM Backend Module ver 0.1 (PID %s)" % (os.getpid()))
+
     if data[0] == "PING":
         responder("END")
+
     if data[0] == "AXFR":
         hosts = api.zone(domain)
+
         if hosts == False:
             responder("FAIL")
             continue
-        responder("DATA\t%s\tIN\tSOA\t3600\t-1\t%s %s %s 1800 3600 604800 3600" % (domain,dns_server[0],config.get('dns','email'),time.strftime('%Y%m%d%H')))
+
+        responder("DATA\t%s\tIN\tSOA\t3600\t-1\t%s %s %s 1800 3600 604800 3600" % (
+            domain,
+            dns_server[0],
+            config.get('dns','email'),
+            time.strftime('%Y%m%d%H'))
+        )
 
         i = 1 
         for server in dns_server:
-            responder("DATA\t%s\tIN\tNS\t3600\t%s\t%s" % (domain,i,server))
-            i=i+1
+            responder("DATA\t%s\tIN\tNS\t3600\t%s\t%s" % (
+                domain,
+                i,
+                server)
+            )
+
+            i+=1
 
         for host in hosts:
             if domain.endswith('in-addr.arpa'):
                 ip = host['ip'].split('.')
-                arpa = "%s.%s.%s.%s.in-addr.arpa" % (ip[3],ip[2],ip[1],ip[0])
-                responder("DATA\t%s\tIN\tPTR\t3600\t-1\t%s" % (arpa,host['host']))
+                arpa = "%s.%s.%s.%s.in-addr.arpa" % (ip[3], ip[2], ip[1], ip[0])
+                responder("DATA\t%s\tIN\tPTR\t3600\t-1\t%s" % (arpa, host['host']))
+
             elif domain.endswith('ip6.arpa'):
-                responder("DATA\t%s\tIN\tPTR\t3600\t-1\t%s" % (host['ip6_rev'],host['host']))
+                responder("DATA\t%s\tIN\tPTR\t3600\t-1\t%s" % (host['ip6_rev'], host['host']))
+
             else:
                 if 'ip' in host.keys():
-                    responder("DATA\t%s\tIN\tA\t3600\t-1\t%s" % (host['host'],host['ip']))
+                    responder("DATA\t%s\tIN\tA\t3600\t-1\t%s" % (host['host'], host['ip']))
+
                 elif 'ip6' in host.keys():
-                    responder("DATA\t%s\tIN\tAAAA\t3600\t-1\t%s" % (host['host'],host['ip6']))
+                    responder("DATA\t%s\tIN\tAAAA\t3600\t-1\t%s" % (host['host'], host['ip6']))
+
         responder("END")
  
     if data[0] == "Q":
@@ -76,37 +102,46 @@ while 1:
                 logging.error("No records");
                 responder("FAIL")
                 continue
+
             else:
                 hosts = []
+
         domain = data[1]
 
         if data[3] == "SOA" or data[3] == "ANY":
-            responder("DATA\t%s\tIN\tSOA\t3600\t-1\t%s %s %s 1800 3600 604800 3600" % (data[1],dns_server[0],config.get('dns','email'),time.strftime('%Y%m%d%H')))
+            responder("DATA\t%s\tIN\tSOA\t3600\t-1\t%s %s %s 1800 3600 604800 3600" % (
+                data[1],
+                dns_server[0],
+                config.get('dns','email'),
+                time.strftime('%Y%m%d%H'))
+            )
 
         if data[3] == "NS" or data[3] == "ANY":
             i=1
+
             for server in dns_server:
-                responder("DATA\t%s\tIN\tNS\t3600\t%s\t%s" % (data[1],i,server))
-                i=i+1
+                responder("DATA\t%s\tIN\tNS\t3600\t%s\t%s" % (data[1], i, server))
+
+                i+=1
 
         if (data[3] == "A" or data[3] == "ANY") and not data[1].endswith('in-addr.arpa'):
             for host in hosts:
                 if 'ip' in host.keys():
-                    responder("DATA\t%s\tIN\tA\t3600\t-1\t%s" % (data[1],host['ip']))
+                    responder("DATA\t%s\tIN\tA\t3600\t-1\t%s" % (data[1], host['ip']))
 
         if (data[3] == "AAAA" or data[3] == "ANY") and not data[1].endswith('in-addr.arpa'):
             for host in hosts:
                 if 'ip6' in host.keys():
-                    responder("DATA\t%s\tIN\tAAAA\t3600\t-1\t%s" % (data[1],host['ip6']))
+                    responder("DATA\t%s\tIN\tAAAA\t3600\t-1\t%s" % (data[1], host['ip6']))
 
         if (data[3] == "PTR" or data[3] == "ANY") and data[1].endswith('in-addr.arpa'):
             for host in hosts:
                 ip = host['ip'].split('.')
-                arpa = "%s.%s.%s.%s.in-addr.arpa" % (ip[3],ip[2],ip[1],ip[0])
-                responder("DATA\t%s\tIN\tPTR\t3600\t-1\t%s" % (arpa,host['host']))
+                arpa = "%s.%s.%s.%s.in-addr.arpa" % (ip[3], ip[2], ip[1], ip[0])
+                responder("DATA\t%s\tIN\tPTR\t3600\t-1\t%s" % (arpa, host['host']))
 
         if (data[3] == "PTR" or data[3] == "ANY") and data[1].endswith('ip6.arpa'):
             for host in hosts:
-                responder("DATA\t%s\tIN\tPTR\t3600\t-1\t%s" % (data[1],host['host']))
-                #do something with the hosts
+                responder("DATA\t%s\tIN\tPTR\t3600\t-1\t%s" % (data[1], host['host']))
+
         responder("END")
